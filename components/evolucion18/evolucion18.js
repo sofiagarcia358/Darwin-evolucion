1648dcc3950d26dfd77def59a2525dbc220ad828
sagarcia <sagarcia@scl.edu.gtgit config --global user.email sagarcia@scl.edu.gt>
2024-06-20 11:39:23 -0600

    version2-sofia

--- components/evolucion18/evolucion18.js
+++ components/evolucion18/evolucion18.js
@@ -4,42 +4,42 @@ DOM_evolucion18.innerHTML = `
 <h1 class="titulo">Evolucion de la <span>Arquitectura:</span></h1>
 <section class="arquitectura">
 
-<div class="p1">1.  Arquitectura Antigua:
+<div class="p1sofia">1.  Arquitectura Antigua:
 <img class="img1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVZfwV5HPSu-RlMyfHIpnFtAGW5UigWINRpA&usqp=CAU" alt="">
     <span class="info1">
     La arquitectura antigua es un término que se refiere a las construcciones y estilos arquitectónicos que surgieron en las diferentes civilizaciones antiguas del mundo.
     </span>
 </div>
 
-<div class="p2">2.  Arquitectura Medieval:
+<div class="p2sofia">2.  Arquitectura Medieval:
 <img class="img2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzfhWLEzwPabLclr2xwOl9OI1PCrF_crCtfk4dL07WJBIvRsydi5rcbPND7nUc--CLNP0&usqp=CAU" alt="">
 <span class="info2">
     las construcciones realizadas en Europa durante los siglos V al XV. Caracterizada por catedrales góticas, castillos fortificados y monasterios románicos, refleja la influencia religiosa, y la maestría en la talla de piedra
 </span>
 </div>
 
-<div class="p3">3.  Arquitectura del Renacimiento y Barroco:
+<div class="p3sofia">3.  Arquitectura del Renacimiento y Barroco:
 <img class="img3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Les_Invalides_in_Paris.jpg/480px-Les_Invalides_in_Paris.jpg" alt="">
 <span class="info3">
   El Renacimiento (1400-1500) se inspiró en el arte clásico griego y romano, manteniendo la simetría y proporción con pocos detalles. El Barroco (1500-1600) se caracterizó por el uso de líneas curvas y el uso de mucho detalle para crear conjuntos artísticos
 </span>
 </div>
 
-<div class="p4">4.  Arquitectura Moderna:
+<div class="p4sofia">4.  Arquitectura Moderna:
 <img class="img4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/6265_Dessau.JPG/220px-6265_Dessau.JPG" alt="">
   <span class="info4">
   Estilo arquitectonico que domino el mundo durante 1930-1960, se caracterizo por un enfoque analítico y funcional del diseño de edificios. Los edificios de este estilo suelen tener tejados planos y ventanas con cortinas.
   </span>
 </div>
 
-<div class="p5">5.  Arquitectura Contemporánea:
+<div class="p5sofia">5.  Arquitectura Contemporánea:
 <img class="img5" src="https://a-cero.com/wp-content/uploads/2022/08/Balcony-House-Majadahonda-es-un-ejemplo-de-mayor-libertad-de-diseno-en-la-arquitectura-contemporanea-.jpg" alt="">
 <span class="info5">
 Esta se centra en la creación de edificios que reflejen las necesidades y deseos de la sociedad actual. Conjunto de estilos arquitectonicos, de los últimos 30 años, por eso vemos  edificios que rompen con la arquitectura tradicional.
 </span>
 </div>
 </section>
-<footer class="f18s">Arquitectura <span> @Sofía.</span></footer>
+
 
 </section>
 `;
